b5d244a01a679e8d6a95953a90d7968740f09064
diego <145928215+vpsvip7@users.noreply.github.com>
2024-02-04 19:39:20 -0300

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,8 @@ sudo touch /etc/UDPCustom/udp-custom
 udp_dir='/etc/UDPCustom'
 udp_file='/etc/UDPCustom/udp-custom'
 
-sudo apt install -y update
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt install -y wget
 sudo apt install -y curl
 sudo apt install -y dos2unix
